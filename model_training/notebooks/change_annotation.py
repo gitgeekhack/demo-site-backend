7a8c6eb4b298aaf32e423d86028be9f681ca56c2
gitgeekhack <64969815+gitgeekhack@users.noreply.github.com>
2022-04-11 11:42:04 +0530

    added model with date label (#4)

--- model_training/notebooks/change_annotation.py
+++ model_training/notebooks/change_annotation.py
@@ -11,12 +11,10 @@ for filename in tqdm(os.listdir(directory)):
     df = df.dropna(axis=1)
     df.columns = ['label', 'x0', 'y0', 'x1', 'y1']
 
-    # print(df['label'])
     df['label'] = np.where(df['label'] ==4, 3, df['label'])
     df['label'] = np.where(df['label'] == 5, 3, df['label'])
     df['label'] = np.where(df['label'] >= 6, df['label']-2, df['label'])
-    # print(df['label'])
-    # print(df)
+
     with open(f'/home/heli/Desktop/git/demo-site-backend/data/driving_license/new_labels/{filename}', 'a') as f:
         dfAsString = df.to_string(header=False, index=False)
         f.write(dfAsString)
