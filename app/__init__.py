ce8fe6e5317971b3403fc36029ca1893d5db9480
gitgeekhack <64969815+gitgeekhack@users.noreply.github.com>
2022-05-04 16:59:14 +0530

    updated changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,9 @@
-import os
-
-import aiohttp_jinja2
-
 from app.manage import create_app
-from aiohttp import web
-import jinja2
 
 app, logger = create_app()
 
 from app.resource.pinger import Pinger
-from app.resource.driving_license.extract import Index, DLExtract
+from app.resource.driving_license.extract import Index, DLExtractor
 
 app.router.add_view('/', Index)
-app.router.add_view('/dlocr', DLExtract)
+app.router.add_view('/dl-ocr', DLExtractor)
