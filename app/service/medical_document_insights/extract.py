5a6760647f4c0d72e21f3178279f8d866c31324d
gitgeekhack <64969815+gitgeekhack@users.noreply.github.com>
2023-11-03 15:02:24 +0530

    add medical poc file structure

---
+++ app/service/medical_document_insights/extract.py
@@ -0,0 +1,12 @@
+from .nlp_extractor import *
+
+
+class DocumentInsightExtractor:
+
+    def __init__(self, document):
+        self.document = document
+
+    async def extract_insights(self):
+        # extractor = ComprehendMedicalExtractor()
+        # result = extractor.pagewise_entity_extractor(text)
+        pass
