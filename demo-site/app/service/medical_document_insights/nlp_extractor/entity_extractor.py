2543f7a16f9a1441576bbb2fc8e0c6fde336bb1c
gitgeekhack <64969815+gitgeekhack@users.noreply.github.com>
2023-12-26 15:09:20 +0530

    handle the exception in entity

--- demo-site/app/service/medical_document_insights/nlp_extractor/entity_extractor.py
+++ demo-site/app/service/medical_document_insights/nlp_extractor/entity_extractor.py
@@ -36,6 +36,11 @@ async def convert_str_into_json(text):
     start_index = text.find('{')
     end_index = text.rfind('}') + 1
     json_str = text[start_index:end_index]
+
+    if len(json_str) == 0:
+        final_data = {'diagnosis': [], 'treatments': [], 'medications': []}
+        return final_data
+
     data = json.loads(json_str)
     data_keys = ['diagnosis', 'treatments', 'medications']
     final_data = dict(zip(data_keys, list(data.values())))
@@ -115,7 +120,6 @@ async def get_extracted_entities(json_data):
     """ This method is used to provide medical entities from document"""
 
     entity = {}
-    page_wise_entities = {}
 
     task = []
     with futures.ProcessPoolExecutor(os.cpu_count() - 1) as executor:
