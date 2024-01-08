f24c96443551846b91281f8c7ff30793f75001ca
gitgeekhack <64969815+gitgeekhack@users.noreply.github.com>
2024-01-08 16:14:40 +0530

    updated response header in COT, DL, and BD

--- demo-site/app/resource/barcode_extraction/extract_barcode.py
+++ demo-site/app/resource/barcode_extraction/extract_barcode.py
@@ -4,14 +4,15 @@ import json
 
 import aiohttp_jinja2
 from aiohttp import web
-from app.common.utils import is_image_file, get_file_from_path, get_file_size
 from app.service.barcode_extraction.extract_barcode import BarcodeExtraction
+from app.common.utils import is_image_file, get_file_from_path, get_file_size, get_response_headers
 from app.business_rule_exception import InvalidFile, FileLimitExceeded, FilePathNull, MultipleFileUploaded, MissingRequestBody
 
 
 class BarCodeExtraction:
     async def post(self):
         x_uuid = uuid.uuid1()
+        headers = await get_response_headers()
         filedata = []
         try:
             data_bytes = await self.content.read()
@@ -42,31 +43,31 @@ class BarCodeExtraction:
                 message = {
                     "message": "barcode is not scannable"
                 }
-                return web.json_response(message, status=422)
-            return web.json_response({'data': data}, status=200)
+                return web.json_response(message, headers=headers, status=422)
+            return web.json_response({'data': data}, headers=headers, status=200)
 
         except FilePathNull as e:
             response = {"message": f"{e}"}
-            return web.json_response(response, status=400)
+            return web.json_response(response, headers=headers, status=400)
 
         except MultipleFileUploaded as e:
             response = {"message": f"{e}"}
-            return web.json_response(response, status=400)
+            return web.json_response(response, headers=headers, status=400)
 
         except FileNotFoundError:
             response = {"message": "File Not Found"}
-            return web.json_response(response, status=404)
+            return web.json_response(response, headers=headers, status=404)
 
         except FileLimitExceeded as e:
             response = {"message": f"{e}"}
-            return web.json_response(response, status=400)
+            return web.json_response(response, headers=headers, status=400)
 
         except InvalidFile:
             response = {"message": "Unsupported Media Type, Only JPG, JPEG, and PNG formats are Supported!"}
-            return web.json_response(response, status=415)
+            return web.json_response(response, headers=headers, status=415)
 
         except Exception as e:
             print(f'Request ID: [{x_uuid}] %s -> %s', e, traceback.format_exc())
             response = {"message": "Internal Server Error"}
             print(f'Request ID: [{x_uuid}] Response: {response}')
-            return web.json_response(response, status=500)
+            return web.json_response(response, headers=headers, status=500)
