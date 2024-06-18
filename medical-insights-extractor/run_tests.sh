25921401a1dc7a97b1e706f16cf76cf187d4c09a
gitgeekhack <64969815+gitgeekhack@users.noreply.github.com>
2024-06-18 18:42:57 +0530

    changed the access path

--- medical-insights-extractor/run_tests.sh
+++ medical-insights-extractor/run_tests.sh
@@ -4,14 +4,14 @@
 DOCKER_COMMAND="$@"
 
 # Run tests
-cd tests
+cd ../medical-insights-extractor/tests
 echo "Running tests..."
 pytest
 
 # Check the exit status of pytest
 if [ $? -eq 0 ]; then
     echo "Tests passed successfully. Proceeding to build Docker image."
-    cd ..
+    cd ../../demo-site
     eval "$DOCKER_COMMAND"
 else
     echo "Tests failed. Aborting Docker image build."
